d216193bf3f6af67c926cfd9921d21c05fbb48c4
pinpong <o.nitzschke@gmail.com>
2016-12-29 18:27:51 +0100

    renamed

--- src/main/java/jota/dto/response/GetTransferResponse.java
+++ src/main/java/jota/dto/response/GetTransferResponse.java
@@ -1,25 +1,21 @@
 package jota.dto.response;
 
 import jota.model.Bundle;
-import jota.model.Transfer;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by pinpong on 28.12.16.
  */
 public class GetTransferResponse  extends AbstractResponse {
 
-    private Bundle[] transfers;
+    private Bundle[] transferBundle;
 
-    public static GetTransferResponse create(Bundle[] transfers) {
+    public static GetTransferResponse create(Bundle[] transferBundle) {
         GetTransferResponse res = new GetTransferResponse();
-        res.transfers = transfers;
+        res.transferBundle = transferBundle;
         return res;
     }
 
     public Bundle[]  getTransfers() {
-        return transfers;
+        return transferBundle;
     }
 }
