1b8be4ed94758dccc036a777ced66f1963b271ba
adrianziser <adrian@ziser.it>
2017-02-18 21:56:33 +0100

    update javadoc deployment script

--- .travis/push-jdoc-gh-pages.sh
+++ .travis/push-jdoc-gh-pages.sh
@@ -9,7 +9,7 @@ if [ "$TRAVIS_REPO_SLUG" == "iotaledger/iota.lib.java" ] && [ "$TRAVIS_PULL_REQU
   cd $HOME
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "travis-ci"
-  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/iotaledger/iota.lib.java gh-pages > /dev/null
+  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/iotaledger/iota.lib.java.git gh-pages > /dev/null
 
   cd gh-pages
   git rm -rf ./javadoc
