b495e6d4a9c5cc51a9877fe9c1d867964499cff4
Brord van Wierst <brordvanwierst@msn.com>
2019-02-16 22:04:22 +0100

    Added some documentation to config classes to explain their usage

--- jota/src/main/java/org/iota/jota/config/IotaClientConfig.java
+++ jota/src/main/java/org/iota/jota/config/IotaClientConfig.java
@@ -6,6 +6,15 @@ import java.util.List;
 import org.iota.jota.connection.Connection;
 import org.iota.jota.store.Store;
 
+/**
+ * 
+ * IotaClientConfig is a config which implements all required config variables using a provided storage method.
+ * It contains utility methods for getting fields from the storage by key.
+ * 
+ * We have this because each Config type( {@link EnvConfig}, {@link FileConfig}) have their own naming conventions
+ * but use the same reading system through their storage object
+ *
+ */
 public abstract class IotaClientConfig implements IotaConfig {
     
     protected Store store;
@@ -17,6 +26,13 @@ public abstract class IotaClientConfig implements IotaConfig {
         
     }
 
+    /**
+     * Creates a new IotaClientConfig using the provided storage method.
+     * The store gets loaded as well.
+     * 
+     * @param store the store to use in this config
+     * @throws Exception if the store failed to load
+     */
     public IotaClientConfig(Store store) throws Exception {
         this.store = store;
         this.store.load();
@@ -33,14 +49,21 @@ public abstract class IotaClientConfig implements IotaConfig {
         return nodes != null && nodes.size() > 0;
     }
 
+    /**
+     * Reads the value from the store using the provided key
+     * 
+     * @param key the store field we are reading
+     * @return The value or <code>null</code>
+     */
     protected String stringOrNull(String key) {
         Serializable ret = store.get(key);
         return ret != null ? ret .toString() : null;
     }
     
     /**
+     * Reads the value from the store using the provided key
      * 
-     * @param key
+     * @param key the store field we are reading
      * @return The parsed int value or <code>0</code>
      */
     protected int intOrNull(String key) {
--- jota/src/main/java/org/iota/jota/config/IotaConfig.java
+++ jota/src/main/java/org/iota/jota/config/IotaConfig.java
@@ -1,5 +1,10 @@
 package org.iota.jota.config;
 
+/**
+ * 
+ * An interface which captures all config options iota-java has available.
+ *
+ */
 public interface IotaConfig extends ApiConfig, AccountConfig {
-
+    
 }
